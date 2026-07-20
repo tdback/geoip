83128149b0d57aa37cb8f132b0ad50bcfc20d589
tdback <tyler@tdback.net>
2026-07-20 15:20:53 -0400

    add devShell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs =
+    { self, ... }@inputs:
+    let
+      systems = [ "x86_64-linux" ];
+      eachSystem = inputs.nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          overlays = [ (import inputs.rust-overlay) ];
+          pkgs = import inputs.nixpkgs {
+            inherit system overlays;
+          };
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [ rust-bin.stable.latest.default ];
+          };
+        }
+      );
+    };
+}
